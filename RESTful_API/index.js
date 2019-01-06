db87303fcf81149dd8fdf7b543bf17077e6e0233
Rahul Nandrajog <2016.rahul.nandrajog@ves.ac.in>
2019-01-06 11:16:47 +0530

    handlers object and route written

--- RESTful_API/index.js
+++ RESTful_API/index.js
@@ -8,6 +8,8 @@
 var http = require('http');
 // For getting the url you need the URL module of node 
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+
 
 // The server should respond to all the request with a string
 // create a server variable with a callback function taking 2 parameters res(response) and req(request) 
@@ -32,12 +34,24 @@ var server = http.createServer(function(req,res){
     // Get the headers as an object 
     var headers = req.headers;
 
-    // Send the response 
-    res.end('hello World ! \n');
+    // Get the payloads, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer ='';
+    req.on('data',function(data){
+        buffer += decoder.write(data);
+    }); 
 
-    // Log the request path 
-    console.log('Request received with these headers: ',headers);
+    // to end the endless stream of data we call an event known as the end event 
+    req.on('end',function(){
+        buffer += decoder.end();
 
+        // Send the response 
+        res.end('hello World ! \n');
+
+        // Log the request path 
+        console.log('Request received with this payloads is: ',buffer);
+
+    });
 });
 
 // Start the server , and have a listen on port 3000 
@@ -46,3 +60,26 @@ server.listen(3000,function(){
 });
 
 // for putting manual headers used the mod headers extension of the chrome library 
+
+// after defining the handler object, we have to modify the server in such a way that it figures out which handler to call when 
+// This modification is done in req.on('end') as everything with or without a payload executes this 
+// In the req.on('end') the logic is that if the trimmed path is found on router then go to handler method called by the router else go to the notfound router 
+
+// Define handler object 
+var handlers ={};
+
+// define the handler.sample method 
+handlers.sample=function(data, callback){
+    // Callback an http status code and a payload object 
+    callback(406,{'name' : 'sample-handler'});
+};
+// define the non found handler 
+handlers.notfound= function(data,callback){
+    // Call back the not found status code 
+    callback(404);
+}
+// you need a router object to parse a particular URL and make api more responsve 
+var router = {
+    // define the handler variable 
+    'sample' : handlers.sample
+};
